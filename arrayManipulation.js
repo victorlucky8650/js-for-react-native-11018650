ecf113136c24ad5739f800cc22c961cb27e91096
victorlucky8650 <vldawu@st.edu.gh.com>
2024-06-27 12:58:48 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,6 +10,18 @@ console.log('Task 1 Output: ', processArray(numbers));
 
 
 
+// Task 2:
+
+// Function to format array strings
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+        return processedNumbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
+
+let strings = ["This", "is", "Victor", "Lucky", "Dawu"];
+let processedNumbers = processArray(numbers);
+console.log('Task 2 Output: ', formatArrayStrings(strings, processedNumbers)); 
 
 
 
